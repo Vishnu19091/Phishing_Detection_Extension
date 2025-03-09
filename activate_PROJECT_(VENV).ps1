2d3d219120fe91b27523f66aa681cd82185da88b
Vishnu_1909 <143880481+Vishnu19091@users.noreply.github.com>
2025-03-09 16:22:23 +0530

    Update activate_PROJECT_(VENV).ps1

--- activate_PROJECT_(VENV).ps1
+++ activate_PROJECT_(VENV).ps1
@@ -1,6 +1,6 @@
 # activate_venv.ps1
 # Change directory to the location of the virtual environment
-Set-Location -Path "D:\clg\Project Phase 1\Project\Project-ENV\Scripts\" #add your path to venv directory 
+Set-Location -Path "PATH_TO_THE_VENV_DIR" #add your path to venv directory 
 
 # Activate the virtual environment
 .\Activate.ps1
