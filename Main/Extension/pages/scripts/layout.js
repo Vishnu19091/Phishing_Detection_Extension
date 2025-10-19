f2c3e5f7a30161d39d566ba1812cd2d26529a760
Vishnu19091 <vishnusah368@gmail.com>
2025-10-19 14:18:31 +0530

    Test Extension Page Completed & Updated

--- Main/Extension/pages/scripts/layout.js
+++ Main/Extension/pages/scripts/layout.js
@@ -1,34 +1,5 @@
 "use strict";
 
-/*<----------- FOOTER----------->
-
-<footer class="text-center py-4 text-gray-400 text-sm">
-  © 2025 Anti-Phishing Extension
-</footer>
-
-*/
-
-/*<----------- SIDEBAR ----------->
-<header>
-  <div class="absolute left-1">
-    <div class="flex flex-row gap-4 items-center">
-      <img src="../../icons/icon128.png" width="50" height="50" alt="logo" />
-      <h1 class="text-4xl text-red-600 hover:underline underline-offset-2">
-        Anti-Phishing
-      </h1>
-    </div>
-
-    <a class="text-2xl hover:text-red-600" href="../block_domain.html"
-      >Manage Blocked Domains</a
-    >
-    <a class="text-2xl hover:text-red-600" href="../test.html"
-      >Test Extension</a
-    >
-    <a class="text-2xl hover:text-red-600" href="../about.html">About</a>
-  </div>
-</header>
-*/
-
 const sidebarContainer = document.getElementById("sidebar");
 const footerContainer = document.getElementById("footer");
 
@@ -40,7 +11,7 @@ sidebarContainer.innerHTML = `<header class="absolute left-1">
       </h1>
     </div>
 
-    <div class="flex flex-col gap-4 items-start mt-5">
+    <div class="flex flex-col gap-4 items-start mt-5 ml-4">
     <a class="text-4xl hover:text-red-600 w-fit" href="/pages/block_domain.html"
     >Manage Blocked Domains</a
     >
